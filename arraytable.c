0316d10a9197dfa3044dc7e6d72700ec96f85b37
Va1p3n <88006936+Va1p3n@users.noreply.github.com>
2024-02-14 14:45:19 +0100

    started arraytable

---
+++ arraytable.c
@@ -0,0 +1,5 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#include <table.h>
+#include <array_1d.h>
--- mtftable.c
+++ mtftable.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 #include <table.h>
 #include <dlist.h>
