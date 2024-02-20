fa526cc8bb5afa7051287190b1d5e84deb997a7f
Va1p3n <88006936+Va1p3n@users.noreply.github.com>
2024-02-20 09:56:01 +0100

    for fun

---
+++ timeit-extreme.sh
@@ -0,0 +1,25 @@
+rm testlogs/ttiming.txt testlogs/mtiming.txt testlogs/atiming.txt
+touch testlogs/ttiming.txt testlogs/mtiming.txt testlogs/atiming.txt
+
+for i in 10000 15000 20000 25000 30000 35000 40000;
+do
+    echo
+    echo Running sample size $i...
+    for j in 1 2 3 4 5 6;
+    do
+	echo Running tabletest $j/6...
+	./tabletest -t -n $i >> testlogs/ttiming.txt
+    done
+
+    for j in 1 2 3 4 5 6;
+    do
+	echo Running mtftabletest $j/6...
+	./mtftabletest -t -n $i >> testlogs/mtiming.txt
+    done
+
+    for j in 1 2 3 4 5 6;
+    do
+	echo Running arraytabletest $j/6...
+	./arraytabletest -t -n $i >> testlogs/atiming.txt
+    done
+done
